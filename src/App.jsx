8581ee2830c7b58984fd509be9eac5387edb7b92
abdoubch <la_bouchendouka@esi.dz>
2023-09-14 16:55:59 +0200

    implement hero section: done

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,21 @@ import {
 } from "./components";
 
 const App = () => {
-  return(
+  return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <NavBar/>
-          <Hero/>
+          <NavBar />
+          <Hero />
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <Feedbacks/>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
         <div className="relative z-0">
-          <Contact/>
-          <StarsCanvas/>
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
